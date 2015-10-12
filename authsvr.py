f542a37da480c78fe07a92766b7b2eb663c9b9b5
val <jmward@gmail.com>
2015-10-12 18:48:10 -0400

    add CORS and .gitignore

--- authsvr.py
+++ authsvr.py
@@ -9,11 +9,17 @@ virtualenv .ve
 """
 from gevent import monkey;monkey.patch_all()
 import json
-from bottle import route, run, template, request, response
+from bottle import route, run, template, request, response, hook
 from bottle import abort, redirect, default_app, HTTPResponse
 from uuid import uuid1, uuid4
 
 from config_auth import Config
+
+@hook('after_request')
+def enable_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
 class AuthThing(object):
     def refresh_facebook(_, **kw):
